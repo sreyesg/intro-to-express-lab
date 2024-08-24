4126e21666d5e0d8d9711251a816b9798720cf8c
sreyesg <santiagoreyes.g@gmail.com>
2024-08-24 12:38:11 -0700

    code greet route

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
-
+// Greet Route 
+app.get('/greetings/:username', (req, res) => {
+    const username = req.params.username
+    res.send(`<h1>what a delight to see you once more, ${username}</h1>`)
+})
 
 
 app.listen(3000, () => {
